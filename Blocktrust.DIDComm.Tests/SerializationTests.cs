33c4bd229e3ea65a76d202c1b51e455dd54f921e
Björn Sandmann <sandmann@codedata.solutions>
2023-04-13 16:33:28 +0200

    Extended test

--- Blocktrust.DIDComm.Tests/SerializationTests.cs
+++ Blocktrust.DIDComm.Tests/SerializationTests.cs
@@ -33,14 +33,68 @@ public class SerializationTests
             .from(ALICE_DID)
             .to(new List<string> { BOB_DID })
             .build();
-        
-       var serializedMessage = JsonSerializer.Serialize(message, SerializationOptions.UnsafeRelaxedEscaping); 
-       var unserializedMessage = JsonSerializer.Deserialize<Blocktrust.DIDComm.Message.Messages.Message>(serializedMessage, SerializationOptions.UnsafeRelaxedEscaping);
 
-       unserializedMessage.Attachments.Count.Should().Be(2);
-       unserializedMessage.Attachments[0].Id.Should().Be("1");
-       unserializedMessage.Attachments[0].Data.ToJsonObject().ToJsonString().Should().Be("""{"jws":null,"hash":null,"base64":"SGVsbG8sIHdvcmxk"}""");
-       unserializedMessage.Attachments[1].Id.Should().Be("2");
-       unserializedMessage.Attachments[1].Data.ToJsonObject().ToJsonString().Should().Be("""{"jws":null,"hash":null,"json":{"foo":"bar"}}""");
+        var serializedMessage = JsonSerializer.Serialize(message, SerializationOptions.UnsafeRelaxedEscaping);
+        var unserializedMessage = JsonSerializer.Deserialize<Blocktrust.DIDComm.Message.Messages.Message>(serializedMessage, SerializationOptions.UnsafeRelaxedEscaping);
+
+        unserializedMessage.Attachments.Count.Should().Be(2);
+        unserializedMessage.Attachments[0].Id.Should().Be("1");
+        unserializedMessage.Attachments[0].Data.ToJsonObject().ToJsonString().Should().Be("""{"jws":null,"hash":null,"base64":"SGVsbG8sIHdvcmxk"}""");
+        unserializedMessage.Attachments[1].Id.Should().Be("2");
+        unserializedMessage.Attachments[1].Data.ToJsonObject().ToJsonString().Should().Be("""{"jws":null,"hash":null,"json":{"foo":"bar"}}""");
+    }
+
+    [Fact]
+    public async Task Message_with_attachments_serializes_correctly_2()
+    {
+        var didComm = new DidComm(new DidDocResolverMock(), new AliceSecretResolverMock());
+
+        var otherStuff = new Dictionary<string, object>
+        {
+            { "challenge", "x" },
+            { "nonce", "y" },
+            { "domain", "z" }
+        };
+        var presentation = new Dictionary<string, object>
+        {
+            { "id", "x" },
+            { "name", "y" },
+            { "purpose", "z" },
+            {
+                "format", new Dictionary<string, object>()
+                {
+                    {
+                        "jwt", new Dictionary<string, object>()
+                        {
+                            { "alg", "abc" },
+                            { "proof_type", null }
+                        }
+                    }
+                }
+            }
+        };
+        var attachments = new List<Attachment>
+        {
+            new AttachmentBuilder("1", new Json(new Dictionary<string, object>
+                {
+                    { "options", otherStuff },
+                    { "presentation_definition", presentation}
+                }))
+                .MediaType("application/json")
+                .Build()
+        };
+
+        var message = Blocktrust.DIDComm.Message.Messages.Message.Builder("12345", new Dictionary<string, object> { { "foo", "bar" } }, "my-protocol/1.0")
+            .attachments(attachments)
+            .from(ALICE_DID)
+            .to(new List<string> { BOB_DID })
+            .build();
+
+        var serializedMessage = JsonSerializer.Serialize(message, SerializationOptions.UnsafeRelaxedEscaping);
+        var unserializedMessage = JsonSerializer.Deserialize<Blocktrust.DIDComm.Message.Messages.Message>(serializedMessage, SerializationOptions.UnsafeRelaxedEscaping);
+
+        unserializedMessage.Attachments.Count.Should().Be(1);
+        unserializedMessage.Attachments[0].Id.Should().Be("1");
+        unserializedMessage.Attachments[0].Data.ToJsonObject().ToJsonString().Should().Be("""{"jws":null,"hash":null,"json":{"options":{"challenge":"x","nonce":"y","domain":"z"},"presentation_definition":{"id":"x","name":"y","purpose":"z","format":{"jwt":{"alg":"abc","proof_type":null}}}}}""");
     }
 }
